691bd6aa53ee07fb522cdde06be29f6f4623879b
Chien-Chi Wang <oiu850714@gmail.com>
2023-03-21 08:13:30 +0800

    improve

--- 001-two-sum.cpp
+++ 001-two-sum.cpp
@@ -3,17 +3,15 @@
 class Solution {
 public:
   std::vector<int> twoSum(std::vector<int> &Nums, int Target) {
-    std::unordered_map<int, int> IndicesOfPrevElements;
-    for (auto SecondNumIdx = 0; SecondNumIdx < Nums.size(); SecondNumIdx++) {
-      auto SecondNum = Nums[SecondNumIdx], FirstNum = Target - SecondNum;
-      if (IndicesOfPrevElements.find(FirstNum) != IndicesOfPrevElements.end()) {
-        return {SecondNumIdx, IndicesOfPrevElements[FirstNum]};
-      } else {
-        IndicesOfPrevElements.emplace(SecondNum, SecondNumIdx);
+    std::unordered_map<int, int> NumIdx;
+    for (int i = 0; i < Nums.size(); i++) {
+      auto Num1 = Nums[i], Num2 = Target - Num1;
+      if (NumIdx.count(Num2)) {
+        return {i, NumIdx.at(Num2)};
       }
+      NumIdx[Num1] = i;
     }
-
-    // Never happen by the assumption of the unique existence of the solution.
-    throw std::exception();
+    // Impossible by the assumption.
+    return {-1, -1};
   }
 };
